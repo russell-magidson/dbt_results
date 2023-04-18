b24baf0a023868ad4d4b6b56deb792e952a69064
russell-magidson <88747432+russell-magidson@users.noreply.github.com>
2023-04-18 10:13:50 -0400

    change name to table_name

--- models/04_dm/dbt-results/dbt_results.sql
+++ models/04_dm/dbt-results/dbt_results.sql
@@ -15,7 +15,7 @@ with empty_table as (
         cast( null as string) as unique_id,
         cast( null as string) as database_name,
         cast( null as string) as schema_name,
-        cast( null as string) as name,
+        cast( null as string) as table_name,
         cast( null as string) as resource_type,
         cast( null as string) as status,
         cast( null as float64) as execution_time,
